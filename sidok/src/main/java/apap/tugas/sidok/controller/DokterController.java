203d902fd2901cef90fd88d577b21ac273e19fd1
Ringgi Cahyo Dwiputra <ringgicahyo@gmail.com>
2019-10-16 19:05:03 +0700

    feat: add static and templates file, modify DokterController.java

--- sidok/src/main/java/apap/tugas/sidok/controller/DokterController.java
+++ sidok/src/main/java/apap/tugas/sidok/controller/DokterController.java
@@ -1,20 +1,23 @@
 package apap.tugas.sidok.controller;
 
+import apap.tugas.sidok.model.DokterModel;
 import apap.tugas.sidok.service.DokterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/dokter")
+//@RequestMapping("/dokter")
 public class DokterController {
     @Autowired
     private DokterService dokterService;
 
     @RequestMapping("/")
     public String home(Model model) {
+        List<DokterModel> listDokter = dokterService.getDokterList();
         model.addAttribute("pagetitle", "Home");
         return "home";
     }
